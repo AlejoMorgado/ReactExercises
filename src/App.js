b403f8d0d415b14a8c29cbcb6e28123f3aa9bda0
Alejandro Morgado <alejo.morgado2@gmail.com>
2023-05-16 22:11:14 -0500

    Create the react setup and create the first component called tittle

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Tittle from './components/Tittle';
+
+function App() {
+  return (
+    <div className="App">
+      <Tittle text="Find your style here" color="black"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tittle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Tittle = ({text, color}) => {
+
+  return (
+    <p style={{color}}>{text}</p>
+  )
+}
+
+export default Tittle
